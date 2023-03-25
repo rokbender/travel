da4d5867bf0847a0d8f4a50120c8a640cb586451
rokbender <126100986+rokbender@users.noreply.github.com>
2023-03-25 17:16:30 +0200

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,7 +1,13 @@
 
 $(document).ready(function(){
-
+   
    $("#menu").click(function(){
     $("#nav").toggle(1000);
 });
+
+$( window ).resize(function() {
+    if($( window ).width()>800){
+        $(".menu_mobile").hide();
+    }
+  });
 });
